02109466f259e4294561c222941bccc0ffc820c3
AnandaFebianiRakhmah <anandafebianirakhmah@gmail.com>
2025-05-16 11:11:21 +0700

    Add files via upload
    Signed-off-by: AnandaFebianiRakhmah <anandafebianirakhmah@gmail.com>

---
+++ kisagold1/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for kisagold1 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kisagold1.settings')
+
+application = get_wsgi_application()
